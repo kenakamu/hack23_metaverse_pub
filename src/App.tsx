d6a30350f0a299d9c96b1721cc3c8aab6b9f494a
Kenichiro Nakamura <kenakamu@microsoft.com>
2023-09-12 05:57:33 +0900

    add table

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {
   PointerDragBehavior,
   AbstractMesh,
 } from "@babylonjs/core";
-import '@babylonjs/loaders/glTF/glTFFileLoader'
+import '@babylonjs/loaders/glTF/2.0/glTFLoader'
 import SceneComponent from "babylonjs-hook"; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
 import * as tables  from './data/tables.json';
@@ -31,12 +31,12 @@ const onSceneReady = (scene: Scene) => {
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
 
-  SceneLoader.ImportMesh("", "./data/", "Chair.glb", scene, function (meshes) {          
-   // scene.createDefaultCameraOrLight(true, true, true);
-    scene.createDefaultEnvironment();    
-    // meshes.map((mesh)=> mesh.scaling = new Vector3(0.04, 0.04, 0.04));
-    // meshes.map((mesh)=> mesh.rotation.y = -Math.PI);
-  });
+  // SceneLoader.ImportMesh("", "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", "chair.glb", scene, function (meshes) {          
+  //  // scene.createDefaultCameraOrLight(true, true, true);
+  //   scene.createDefaultEnvironment();    
+  //   // meshes.map((mesh)=> mesh.scaling = new Vector3(0.04, 0.04, 0.04));
+  //   // meshes.map((mesh)=> mesh.rotation.y = -Math.PI);
+  // });
   
   if (tableList.length === 0){
     for (let table of Array.from(tables)){
@@ -50,7 +50,8 @@ const onSceneReady = (scene: Scene) => {
   light.intensity = 0.7;
 
   // tableList.map((table: TableModel) => { CreateBox(scene, table.name)});
-  //tableList.map((table: TableModel) => { CreateChairAsync(scene, table.name, table.position_x, table.position_y, table.position_z)});
+  tableList.map((table: TableModel) => { CreateChairAsync(scene, table.name, table.position_x, table.position_y, table.position_z)});
+  tableList.map((table: TableModel) => { CreateTableAsync(scene, table.name, table.position_x, table.position_y, table.position_z)});
   MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
 };
 
@@ -77,34 +78,15 @@ function App() {
   );
 }
 
-function CreateBox(scene: Scene, name: string) : Mesh {
-  let box: Mesh = MeshBuilder.CreateBox(name, { size: 2 }, scene);
-  
-  var pointerDragBehavior1 = new PointerDragBehavior({
-    dragPlaneNormal: new Vector3(0, 1, 0),});
-  pointerDragBehavior1.onDragStartObservable.add((event) => {
-    console.log("dragStart");
-    console.log(event);
-  });
-  pointerDragBehavior1.onDragObservable.add((event) => {
-    console.log("drag");
-    console.log(box.position);
-  });
-  pointerDragBehavior1.onDragEndObservable.add((event) => {
-    console.log("dragEnd");
-    console.log(event);
-    box.position.y = 1;
-  });
-  box.addBehavior(pointerDragBehavior1);
-  box.position.y = 1;
-
-  return box;
-}
-
 async function CreateChairAsync(scene: Scene, name: string, position_x: number, position_y: number, position_z: number){
-  let chair = (await SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/Chair/", "Chair.obj", scene, function (meshes) {          
+  
+  let chair = (await SceneLoader.ImportMeshAsync(
+    "", 
+    "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", 
+    "chair.glb",
+     scene, function (meshes) {          
   })).meshes;
-  chair.map((mesh)=> mesh.scaling = new Vector3(0.04, 0.04, 0.04));
+  chair.map((mesh)=> mesh.scaling = new Vector3(2,2,2));
   chair.map((mesh)=> mesh.rotation.y = -Math.PI);
   chair.map((mesh)=> mesh.position = new Vector3(position_x, position_y, position_z));
   
@@ -126,6 +108,38 @@ async function CreateChairAsync(scene: Scene, name: string, position_x: number, 
   // box.position.y = 1;
   
   return chair;
+}
+
+async function CreateTableAsync(scene: Scene, name: string, position_x: number, position_y: number, position_z: number){
+  
+  let table = (await SceneLoader.ImportMeshAsync(
+    "", 
+    "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", 
+    "table.glb",
+     scene, function (meshes) {          
+  })).meshes;
+  table.map((mesh)=> mesh.scaling = new Vector3(2,2,2));
+  table.map((mesh)=> mesh.rotation.y = -Math.PI);
+  table.map((mesh)=> mesh.position = new Vector3(position_x, position_y, position_z));
+  
+  var pointerDragBehavior1 = new PointerDragBehavior({
+    dragPlaneNormal: new Vector3(0, 1, 0),});
+    pointerDragBehavior1.onDragStartObservable.add((event) => {
+     console.log("dragStart");
+     console.log(event);
+  });
+  pointerDragBehavior1.onDragObservable.add((event) => {
+    console.log("drag");
+    console.log(event);
+  });
+  pointerDragBehavior1.onDragEndObservable.add((event) => {
+    console.log("dragEnd");
+    console.log(event);
+  });
+  table.map((mesh)=> mesh.addBehavior(pointerDragBehavior1));
+  // box.position.y = 1;
+  
+  return table;
 }
 
 export default App;
