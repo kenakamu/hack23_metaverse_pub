95488bc551045b6d068ffa095f48221f62965f96
Kenichiro Nakamura <kenakamu@microsoft.com>
2023-09-12 07:18:00 +0900

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-import {TableModel} from './TableModel';
+import { TableModel } from "./TableModel";
+import * as GUI from "@babylonjs/gui/2D";
 import {
+  Nullable,
   ArcRotateCamera,
+  PointerEventTypes,
+  PointerInfo,
   FreeCamera,
   SceneLoader,
   Vector3,
@@ -12,16 +16,24 @@ import {
   Mesh,
   PointerDragBehavior,
   AbstractMesh,
+  GroundMesh,
 } from "@babylonjs/core";
-import '@babylonjs/loaders/glTF/2.0/glTFLoader'
+import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 import SceneComponent from "babylonjs-hook"; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
-import * as tables  from './data/tables.json';
+import * as tables from "./data/tables.json";
 
 const tableList: TableModel[] = [];
 const onSceneReady = (scene: Scene) => {
   // This creates and positions a free camera (non-mesh)
-  const camera = new ArcRotateCamera("camera1",0,0,0, new Vector3(0, 5, -20), scene);
+  const camera = new ArcRotateCamera(
+    "camera1",
+    0,
+    0,
+    0,
+    new Vector3(0, 5, -20),
+    scene
+  );
   camera.wheelPrecision = 50;
   // This targets the camera to scene origin
   camera.setTarget(Vector3.Zero());
@@ -31,38 +43,121 @@ const onSceneReady = (scene: Scene) => {
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
 
-  // SceneLoader.ImportMesh("", "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", "chair.glb", scene, function (meshes) {          
-  //  // scene.createDefaultCameraOrLight(true, true, true);
-  //   scene.createDefaultEnvironment();    
-  //   // meshes.map((mesh)=> mesh.scaling = new Vector3(0.04, 0.04, 0.04));
-  //   // meshes.map((mesh)=> mesh.rotation.y = -Math.PI);
-  // });
-  
-  if (tableList.length === 0){
-    for (let table of Array.from(tables)){
-      tableList.push(new TableModel(table.name, table.position_x, table.position_y, table.position_z));
-    };
+  // read the initial data if not read yet.
+  if (tableList.length === 0) {
+    for (let table of Array.from(tables)) {
+      tableList.push(
+        new TableModel(
+          table.name,
+          table.position_x,
+          table.position_y,
+          table.position_z
+        )
+      );
+    }
   }
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  // tableList.map((table: TableModel) => { CreateBox(scene, table.name)});
-  tableList.map((table: TableModel) => { CreateChairAsync(scene, table.name, table.position_x, table.position_y, table.position_z)});
-  tableList.map((table: TableModel) => { CreateTableAsync(scene, table.name, table.position_x, table.position_y, table.position_z)});
-  MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
+  var ground:GroundMesh = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
+  InitializeAsync(scene);
+  AddUIControl(scene);
+
+  var startingPoint: Nullable<Vector3>;
+  var currentMesh: Nullable<AbstractMesh>;
+
+    var getGroundPosition = function () {
+        var pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) { return mesh == ground; });
+        if (pickinfo.hit) {
+            return pickinfo.pickedPoint;
+        }
+
+        return null;
+    }
+
+    var pointerDown = function (mesh: Nullable<AbstractMesh>) {
+            currentMesh = mesh;
+            console.log("currentMesh");
+            console.log(currentMesh);
+            startingPoint = getGroundPosition();
+            if (startingPoint) { // we need to disconnect camera from canvas
+                setTimeout(function () {
+                    camera.detachControl();
+                }, 0);
+            }
+    }
+
+    var pointerUp = function () {
+        if (startingPoint) {
+            camera.attachControl(canvas, true);
+            startingPoint = null;
+            return;
+        }
+    }
+
+    var pointerMove = function () {
+        if (!startingPoint) {
+            return;
+        }
+        var current = getGroundPosition();
+        if (!current) {
+            return;
+        }
+
+        var diff = current.subtract(startingPoint);
+        currentMesh!.position.addInPlace(diff);
+
+        startingPoint = current;
+
+    }
+
+    scene.onPointerObservable.add((pointerInfo: PointerInfo) => {      		
+        switch (pointerInfo.type) {
+			case PointerEventTypes.POINTERDOWN:
+				if(pointerInfo.pickInfo!.hit && pointerInfo.pickInfo!.pickedMesh != ground) {
+                    pointerDown(pointerInfo.pickInfo!.pickedMesh)
+                }
+				break;
+			case PointerEventTypes.POINTERUP:
+                    pointerUp();
+				break;
+			case PointerEventTypes.POINTERMOVE:          
+                    pointerMove();
+				break;
+        }
+    });
 };
 
 const onRender = (scene: Scene) => {
   // if (box1 !== undefined) {
   //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-
   //   const rpm = 10;
   //   box1.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
   // }
 };
+
+function InitializeAsync(scene: Scene){
+  tableList.map((table: TableModel) => {
+    return CreateChairAsync(
+      scene,
+      table.name,
+      table.position_x,
+      table.position_y,
+      table.position_z
+    );
+  });
+  tableList.map((table: TableModel) => {
+    return CreateTableAsync(
+      scene,
+      table.name,
+      table.position_x,
+      table.position_y,
+      table.position_z
+    );
+  });
+}
 
 function App() {
   return (
@@ -78,23 +173,34 @@ function App() {
   );
 }
 
-async function CreateChairAsync(scene: Scene, name: string, position_x: number, position_y: number, position_z: number){
-  
-  let chair = (await SceneLoader.ImportMeshAsync(
-    "", 
-    "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", 
-    "chair.glb",
-     scene, function (meshes) {          
-  })).meshes;
-  chair.map((mesh)=> mesh.scaling = new Vector3(2,2,2));
-  chair.map((mesh)=> mesh.rotation.y = -Math.PI);
-  chair.map((mesh)=> mesh.position = new Vector3(position_x, position_y, position_z));
-  
+async function CreateChairAsync(
+  scene: Scene,
+  name: string,
+  position_x: number,
+  position_y: number,
+  position_z: number
+) {
+  let chair = (
+    await SceneLoader.ImportMeshAsync(
+      "",
+      "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/",
+      "chair.glb",
+      scene,
+      function (meshes) {}
+    )
+  ).meshes;
+  chair.map((mesh) => (mesh.scaling = new Vector3(2, 2, 2)));
+  chair.map((mesh) => (mesh.rotation.y = -Math.PI));
+  chair.map(
+    (mesh) => (mesh.position = new Vector3(position_x, position_y, position_z))
+  );
+
   var pointerDragBehavior1 = new PointerDragBehavior({
-    dragPlaneNormal: new Vector3(0, 1, 0),});
-    pointerDragBehavior1.onDragStartObservable.add((event) => {
-     console.log("dragStart");
-     console.log(event);
+    dragPlaneNormal: new Vector3(0, 1, 0),
+  });
+  pointerDragBehavior1.onDragStartObservable.add((event) => {
+    console.log("dragStart");
+    console.log(event);
   });
   pointerDragBehavior1.onDragObservable.add((event) => {
     console.log("drag");
@@ -104,29 +210,41 @@ async function CreateChairAsync(scene: Scene, name: string, position_x: number, 
     console.log("dragEnd");
     console.log(event);
   });
-  chair.map((mesh)=> mesh.addBehavior(pointerDragBehavior1));
+  chair.map((mesh) => mesh.addBehavior(pointerDragBehavior1));
   // box.position.y = 1;
-  
+
   return chair;
 }
 
-async function CreateTableAsync(scene: Scene, name: string, position_x: number, position_y: number, position_z: number){
-  
-  let table = (await SceneLoader.ImportMeshAsync(
-    "", 
-    "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/", 
-    "table.glb",
-     scene, function (meshes) {          
-  })).meshes;
-  table.map((mesh)=> mesh.scaling = new Vector3(2,2,2));
-  table.map((mesh)=> mesh.rotation.y = -Math.PI);
-  table.map((mesh)=> mesh.position = new Vector3(position_x, position_y, position_z));
-  
+async function CreateTableAsync(
+  scene: Scene,
+  name: string,
+  position_x: number,
+  position_y: number,
+  position_z: number
+) {
+  let table = (
+    await SceneLoader.ImportMeshAsync(
+      "",
+      "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/",
+      "table.glb",
+      scene,
+      function (meshes) {}
+    )
+  ).meshes;
+  table.map((mesh) => (mesh.scaling = new Vector3(2, 2, 2)));
+  table.map((mesh) => (mesh.rotation.y = -Math.PI));
+  table.map(
+    (mesh) => (mesh.position = new Vector3(position_x, position_y, position_z))
+  );
+
+
   var pointerDragBehavior1 = new PointerDragBehavior({
-    dragPlaneNormal: new Vector3(0, 1, 0),});
-    pointerDragBehavior1.onDragStartObservable.add((event) => {
-     console.log("dragStart");
-     console.log(event);
+    dragPlaneNormal: new Vector3(0, 1, 0),
+  });
+  pointerDragBehavior1.onDragStartObservable.add((event) => {
+    console.log("dragStart");
+    console.log(event);
   });
   pointerDragBehavior1.onDragObservable.add((event) => {
     console.log("drag");
@@ -136,10 +254,46 @@ async function CreateTableAsync(scene: Scene, name: string, position_x: number, 
     console.log("dragEnd");
     console.log(event);
   });
-  table.map((mesh)=> mesh.addBehavior(pointerDragBehavior1));
-  // box.position.y = 1;
-  
+  table.map((mesh) => mesh.addBehavior(pointerDragBehavior1));
+  table.map((mesh) => (mesh.name = name));
   return table;
 }
 
+function AddUIControl(scene: Scene){
+  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+  const addTableButton = GUI.Button.CreateSimpleButton(
+    "addTableButton",
+    "Add Table"
+  );
+  addTableButton.width = "100px";
+  addTableButton.height = "20px";
+  addTableButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  addTableButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  addTableButton.color = "white";
+  addTableButton.background = "blue";
+  addTableButton.fontSize = 12;
+  addTableButton.top = "10px";
+  addTableButton.left = "10px";
+  addTableButton.onPointerClickObservable.add(function () {
+    CreateTableAsync(scene, "table", 0, 0, 0);
+  });
+  const addChiarButton = GUI.Button.CreateSimpleButton(
+    "addChiarButton",
+    "Add Chair"
+  );
+  addChiarButton.width = "100px";
+  addChiarButton.height = "20px";
+  addChiarButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  addChiarButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  addChiarButton.color = "white";
+  addChiarButton.background = "blue";
+  addChiarButton.fontSize = 12;
+  addChiarButton.top = "30px";
+  addChiarButton.left = "10px";
+  addChiarButton.onPointerClickObservable.add(function () {
+    CreateChairAsync(scene, "table", 0, 0, 0);
+  });
+  advancedTexture.addControl(addTableButton);
+  advancedTexture.addControl(addChiarButton);
+}
 export default App;
