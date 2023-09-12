12ce26160b063290023d0298cf337ed19cfd95ce
Jongdae Lim <jongdaelim@microsoft.com>
2023-09-12 10:14:49 -0700

    new file:   src/ObjectDataModel.ts

---
+++ src/ObjectDataModel.ts
@@ -0,0 +1,10 @@
+class ObjectDataModel {
+    constructor(
+      public name: string,
+      public type: string,
+      public position: { x: number; y: number; z: number },
+      public scale: { x: number; y: number; z: number },
+      public rotation: { x: number; y: number; z: number },
+      public memo: string
+    ) {}
+  }
