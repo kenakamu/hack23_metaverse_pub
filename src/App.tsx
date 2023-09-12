df988539d296f3d508673c17cd9a434e7fa09cd8
Kenichiro Nakamura <kenakamu@microsoft.com>
2023-09-12 11:56:08 +0900

    add rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
-import { DataModel } from "./DataModel";
+import { MeshDataModel } from "./MeshDataModel";
 import * as GUI from "@babylonjs/gui/2D";
 import {
+  Axis,
+  Space,
   Nullable,
   ArcRotateCamera,
   PointerEventTypes,
@@ -23,15 +26,19 @@ import {
 import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 import SceneComponent from "babylonjs-hook"; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
-import * as tables from "./data/tables.json";
+import * as tables from "./data/meshes.json";
 
-const dataList: DataModel[] = [];
-var currentMesh: Nullable<AbstractMesh>;
+const dataList: MeshDataModel[] = [];
+let ground: GroundMesh;
+let currentMesh: Nullable<AbstractMesh> = null;
+let camera: ArcRotateCamera;
+let hl: HighlightLayer;
+let canvas: Nullable<HTMLCanvasElement>;
 
 const onSceneReady = (scene: Scene) => {
-  const hl = new HighlightLayer("hl1", scene);
+  hl = new HighlightLayer("hl1", scene);
   // This creates and positions a free camera (non-mesh)
-  const camera = new ArcRotateCamera(
+  camera = new ArcRotateCamera(
     "camera1",
     0,
     0,
@@ -43,7 +50,7 @@ const onSceneReady = (scene: Scene) => {
   // This targets the camera to scene origin
   camera.setTarget(Vector3.Zero());
 
-  const canvas = scene.getEngine().getRenderingCanvas();
+  canvas = scene.getEngine().getRenderingCanvas();
 
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
@@ -52,12 +59,13 @@ const onSceneReady = (scene: Scene) => {
   if (dataList.length === 0) {
     for (let table of Array.from(tables)) {
       dataList.push(
-        new DataModel(
+        new MeshDataModel(
           table.type,
           table.name,
           table.position_x,
           table.position_y,
-          table.position_z
+          table.position_z,
+          table.rotation_y
         )
       );
     }
@@ -67,18 +75,139 @@ const onSceneReady = (scene: Scene) => {
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  var ground: GroundMesh = MeshBuilder.CreateGround(
-    "ground",
-    { width: 20, height: 20 },
-    scene
-  );
-  InitializeAsync(scene);
+  ground = MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
+  LoadMeshAsync(scene);
   AddUIControl(scene);
+  SetupPointerBehavior(scene);
+  SetupMouseWheelBehavior(scene);
+};
 
+const onRender = (scene: Scene) => {
+  // if (box1 !== undefined) {
+  //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
+  //   const rpm = 10;
+  //   box1.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+  // }
+};
+
+function LoadMeshAsync(scene: Scene) {
+  dataList.map((data: MeshDataModel) => {
+    CreateMeshAsync(
+      scene,
+      data.type,
+      data.name,
+      data.position_x,
+      data.position_y,
+      data.position_z,
+      data.rotation_y
+    );
+    return true;
+  });
+}
+
+async function CreateMeshAsync(
+  scene: Scene,
+  type: string,
+  name: string,
+  position_x: number,
+  position_y: number,
+  position_z: number,
+  rotation_y: number
+) {
+  let mesh_model = (
+    await SceneLoader.ImportMeshAsync(
+      "",
+      "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/",
+      `${type}.glb`,
+      scene,
+      function (meshes) {}
+    )
+  ).meshes;
+  mesh_model.map((mesh) => (mesh.scaling = new Vector3(2, 2, 2)));
+  mesh_model.map((mesh) => (mesh.rotation.y = -Math.PI));
+  mesh_model.map(
+    (mesh) => (mesh.position = new Vector3(position_x, position_y, position_z))
+  );
+  mesh_model.map((mesh) => (mesh.name = name));
+
+  var pointerDragBehavior1 = new PointerDragBehavior({
+    dragPlaneNormal: new Vector3(0, 1, 0),
+  });
+  pointerDragBehavior1.onDragStartObservable.add((event) => {
+    console.log("dragStart");
+  });
+  pointerDragBehavior1.onDragObservable.add((event) => {
+    console.log("drag");
+  });
+  pointerDragBehavior1.onDragEndObservable.add((event) => {
+    console.log("dragEnd");
+  });
+  mesh_model.map((mesh) => mesh.addBehavior(pointerDragBehavior1));
+
+  return mesh_model;
+}
+
+function AddUIControl(scene: Scene) {
+  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+  const addTableButton = GUI.Button.CreateSimpleButton(
+    "addTableButton",
+    "Add Table"
+  );
+  addTableButton.width = "100px";
+  addTableButton.height = "20px";
+  addTableButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  addTableButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  addTableButton.color = "white";
+  addTableButton.background = "blue";
+  addTableButton.fontSize = 12;
+  addTableButton.top = "10px";
+  addTableButton.left = "10px";
+  addTableButton.onPointerClickObservable.add(function () {
+    CreateMeshAsync(scene, "table", uuid(), 0, 0, 0, -Math.PI);
+  });
+
+  const addChiarButton = GUI.Button.CreateSimpleButton(
+    "addChiarButton",
+    "Add Chair"
+  );
+  addChiarButton.width = "100px";
+  addChiarButton.height = "20px";
+  addChiarButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  addChiarButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  addChiarButton.color = "white";
+  addChiarButton.background = "blue";
+  addChiarButton.fontSize = 12;
+  addChiarButton.top = "30px";
+  addChiarButton.left = "10px";
+  addChiarButton.onPointerClickObservable.add(function () {
+    CreateMeshAsync(scene, "chair", uuid(), 0, 0, 0, -Math.PI);
+  });
+
+  const removeButton = GUI.Button.CreateSimpleButton("removeButton", "Remove");
+
+  removeButton.width = "100px";
+  removeButton.height = "20px";
+  removeButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  removeButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  removeButton.color = "white";
+  removeButton.background = "blue";
+  removeButton.fontSize = 12;
+  removeButton.top = "50px";
+  removeButton.left = "10px";
+  removeButton.onPointerClickObservable.add(function () {
+    currentMesh?.dispose();
+  });
+
+  advancedTexture.addControl(addTableButton);
+  advancedTexture.addControl(addChiarButton);
+  advancedTexture.addControl(removeButton);
+}
+
+function SetupPointerBehavior(scene: Scene) {
   var startingPoint: Nullable<Vector3>;
   var getGroundPosition = function () {
     var pickinfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) {
-      return mesh == ground;
+      return mesh === ground;
     });
     if (pickinfo.hit) {
       return pickinfo.pickedPoint;
@@ -87,16 +216,23 @@ const onSceneReady = (scene: Scene) => {
     return null;
   };
 
-  var pointerDown = function (mesh: Nullable<AbstractMesh>) {
+  var pointerDownOnMesh = function (mesh: Nullable<AbstractMesh>) {
     hl.removeAllMeshes();
     hl.addMesh(mesh as Mesh, Color3.Green());
     currentMesh = mesh;
+    console.log(currentMesh!.name);
     startingPoint = getGroundPosition();
     if (startingPoint) {
       setTimeout(function () {
         camera.detachControl();
       }, 0);
     }
+  };
+
+  var pointerDownOnGround = function () {
+    hl.removeAllMeshes();
+    currentMesh = null;
+    camera.inputs.addMouseWheel();
   };
 
   var pointerUp = function () {
@@ -127,9 +263,11 @@ const onSceneReady = (scene: Scene) => {
       case PointerEventTypes.POINTERDOWN:
         if (
           pointerInfo.pickInfo!.hit &&
-          pointerInfo.pickInfo!.pickedMesh != ground
+          pointerInfo.pickInfo!.pickedMesh !== ground
         ) {
-          pointerDown(pointerInfo.pickInfo!.pickedMesh);
+          pointerDownOnMesh(pointerInfo.pickInfo!.pickedMesh);
+        } else {
+          pointerDownOnGround();
         }
         break;
       case PointerEventTypes.POINTERUP:
@@ -140,36 +278,17 @@ const onSceneReady = (scene: Scene) => {
         break;
     }
   });
-};
+}
 
-const onRender = (scene: Scene) => {
-  // if (box1 !== undefined) {
-  //   const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-  //   const rpm = 10;
-  //   box1.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
-  // }
-};
-
-function InitializeAsync(scene: Scene) {
-  dataList.map((data: DataModel) => {
-    if (data.type === "chair") {
-      CreateChairAsync(
-        scene,
-        data.name,
-        data.position_x,
-        data.position_y,
-        data.position_z
-      );
-    } else if (data.type === "table") {
-      CreateTableAsync(
-        scene,
-        data.name,
-        data.position_x,
-        data.position_y,
-        data.position_z
-      );
+function SetupMouseWheelBehavior(scene: Scene) {
+  window.addEventListener("wheel", function (event) {
+    if (currentMesh !== null) {
+      camera.inputs.removeByType("ArcRotateCameraMouseWheelInput");
+      var delta = Math.sign(event.deltaY);
+      (currentMesh as Mesh).rotate(Axis.Y, delta * 0.1, Space.WORLD);
+    } else {
+      camera.wheelPrecision = 50;
     }
-    return true;
   });
 }
 
@@ -187,147 +306,4 @@ function App() {
   );
 }
 
-async function CreateChairAsync(
-  scene: Scene,
-  name: string,
-  position_x: number,
-  position_y: number,
-  position_z: number
-) {
-  let chair = (
-    await SceneLoader.ImportMeshAsync(
-      "",
-      "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/",
-      "chair.glb",
-      scene,
-      function (meshes) {}
-    )
-  ).meshes;
-  chair.map((mesh) => (mesh.scaling = new Vector3(2, 2, 2)));
-  chair.map((mesh) => (mesh.rotation.y = -Math.PI));
-  chair.map(
-    (mesh) => (mesh.position = new Vector3(position_x, position_y, position_z))
-  );
-  chair.map((mesh) => (mesh.name = name));
-
-  var pointerDragBehavior1 = new PointerDragBehavior({
-    dragPlaneNormal: new Vector3(0, 1, 0),
-  });
-  pointerDragBehavior1.onDragStartObservable.add((event) => {
-    console.log("dragStart");
-    console.log(event);
-  });
-  pointerDragBehavior1.onDragObservable.add((event) => {
-    console.log("drag");
-    console.log(event);
-  });
-  pointerDragBehavior1.onDragEndObservable.add((event) => {
-    console.log("dragEnd");
-    console.log(event);
-  });
-  chair.map((mesh) => mesh.addBehavior(pointerDragBehavior1));
-
-  return chair;
-}
-
-async function CreateTableAsync(
-  scene: Scene,
-  name: string,
-  position_x: number,
-  position_y: number,
-  position_z: number
-) {
-  let table = (
-    await SceneLoader.ImportMeshAsync(
-      "",
-      "https://raw.githubusercontent.com/kenakamu/hack23_metaverse_pub/main/src/data/",
-      "table.glb",
-      scene,
-      function (meshes) {}
-    )
-  ).meshes;
-  table.map((mesh) => (mesh.scaling = new Vector3(2, 2, 2)));
-  table.map((mesh) => (mesh.rotation.y = -Math.PI));
-  table.map(
-    (mesh) => (mesh.position = new Vector3(position_x, position_y, position_z))
-  );
-
-  var pointerDragBehavior1 = new PointerDragBehavior({
-    dragPlaneNormal: new Vector3(0, 1, 0),
-  });
-  pointerDragBehavior1.onDragStartObservable.add((event) => {
-    console.log("dragStart");
-    console.log(event);
-  });
-  pointerDragBehavior1.onDragObservable.add((event) => {
-    console.log("drag");
-    console.log(event);
-  });
-  pointerDragBehavior1.onDragEndObservable.add((event) => {
-    console.log("dragEnd");
-    console.log(event);
-  });
-  table.map((mesh) => mesh.addBehavior(pointerDragBehavior1));
-  table.map((mesh) => (mesh.name = name));
-  return table;
-}
-
-function AddUIControl(scene: Scene) {
-  const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-  const addTableButton = GUI.Button.CreateSimpleButton(
-    "addTableButton",
-    "Add Table"
-  );
-  addTableButton.width = "100px";
-  addTableButton.height = "20px";
-  addTableButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  addTableButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  addTableButton.color = "white";
-  addTableButton.background = "blue";
-  addTableButton.fontSize = 12;
-  addTableButton.top = "10px";
-  addTableButton.left = "10px";
-  addTableButton.onPointerClickObservable.add(function () {
-    CreateTableAsync(scene, "table", 0, 0, 0);
-  });
-
-  const addChiarButton = GUI.Button.CreateSimpleButton(
-    "addChiarButton",
-    "Add Chair"
-  );  
-  addChiarButton.width = "100px";
-  addChiarButton.height = "20px";
-  addChiarButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  addChiarButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  addChiarButton.color = "white";
-  addChiarButton.background = "blue";
-  addChiarButton.fontSize = 12;
-  addChiarButton.top = "30px";
-  addChiarButton.left = "10px";
-  addChiarButton.onPointerClickObservable.add(function () {
-    CreateChairAsync(scene, "table", 0, 0, 0);
-  });
-
-  const removeButton = GUI.Button.CreateSimpleButton(
-    "removeButton",
-    "Remove"
-  );
-  
-  removeButton.width = "100px";
-  removeButton.height = "20px";
-  removeButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  removeButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-  removeButton.color = "white";
-  removeButton.background = "blue";
-  removeButton.fontSize = 12;
-  removeButton.top = "50px";
-  removeButton.left = "10px";
-  removeButton.onPointerClickObservable.add(function () {
-    currentMesh?.dispose();
-  });
-
-  advancedTexture.addControl(addTableButton);
-  advancedTexture.addControl(addChiarButton);
-  advancedTexture.addControl(removeButton);
-}
 export default App;
